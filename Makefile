727637314010d76a49c652251b60d2f813271c04
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-04-01 22:55:58 +0200

    Divide into columns using bounding boxes

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	GOARCH=amd64 GOOS=linux go build main.go
+	GOARCH=amd64 GOOS=linux go build main.go ocr.go
 	zip function.zip main
 	aws lambda update-function-code --function-name ${LAMBDA_FUNCTION_NAME} --zip-file fileb://function.zip  | cat
--- main.go
+++ main.go
@@ -4,28 +4,13 @@ import (
 	"fmt"
 
 	"encoding/base64"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/textract"
 )
-
-func erro(e error) string {
-	return fmt.Sprintf(
-		`{"error": "%s"}
-`, e.Error())
-}
-
-func succ(s string) string {
-	return fmt.Sprintf(
-		`{"message": "%s"}
-`, s)
-}
-
-type S struct {
-	Content string `json:"content"`
-}
 
 func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	if !req.IsBase64Encoded {
@@ -43,55 +28,70 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "unable to convert base64 to bytes: %s"}`+"\n", err.Error()),
 		}, nil
 	}
-	text, err := extract(b)
+	output, err := extract(b)
 	if err != nil {
 		return &events.APIGatewayProxyResponse{
 			Headers:    map[string]string{"Content-Type": "application/json"},
 			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "unable to extract: %s"}`+"\n", err.Error()),
+			Body:       fmt.Sprintf(`{"error": "failed to extract: %s"}`+"\n", err.Error()),
 		}, nil
 	}
+	table, err := toTable(output)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 400,
+			Body:       fmt.Sprintf(`{"error": "failed to convert to table: %s"}`+"\n", err.Error()),
+		}, nil
+	}
+	bs, err := json.Marshal(table)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 400,
+			Body:       fmt.Sprintf(`{"error": "failed to convert to json: %s"}`+"\n", err.Error()),
+		}, nil
+	}
+	rows := string(bs)
 	return &events.APIGatewayProxyResponse{
 		Headers:    map[string]string{"Content-Type": "application/json"},
 		StatusCode: 200,
-		// Body:       req.Body, // THIS WORKS!!!!!!!!
-		// Body: fmt.Sprintf(`{"base64": "%t"}`, req.IsBase64Encoded),
-		Body: fmt.Sprintf(`{"text": "%s"}`+"\n", text),
-		// Body:       succ(s.Content),
-		// Body: succ(req.Body),
+		Body:       fmt.Sprintf(`{"rows": "%s"}`+"\n", rows),
 	}, nil
-	/*
-		if err != nil {
-			return &events.APIGatewayProxyResponse{
-				Headers:    map[string]string{"Content-Type": "application/json"},
-				StatusCode: 400,
-				Body:       erro(err),
-			}, nil
-		}
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 200,
-			Body:       succ(text),
-		}, nil
-	*/
 }
 
-func extract(b []byte) (string, error) {
+func extract(bs []byte) (*textract.DetectDocumentTextOutput, error) {
 	mySession, err := session.NewSession()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	svc := textract.New(mySession)
 	input := &textract.DetectDocumentTextInput{
-		Document: &textract.Document{Bytes: b},
+		Document: &textract.Document{Bytes: bs},
 	}
 	output, err := svc.DetectDocumentText(input)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return output.String(), nil
+	return output, nil
 }
 
 func main() {
 	lambda.Start(HandleRequest)
 }
+
+/*
+
+	// write table to stdout
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	for _, row := range table {
+		for j, cell := range row {
+			fmt.Fprintf(w, cell)
+			if j < len(row)-1 {
+				fmt.Fprintf(w, "\t")
+			}
+		}
+		fmt.Fprintf(w, "\n")
+	}
+	w.Flush()
+*/
