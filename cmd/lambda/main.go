0c99e8c1cb8e9ebeb9e87765472318f03bfc7098
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-04-03 19:41:09 +0200

    Extract into packages and add two cmds

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/textract"
+	"github.com/vegarsti/extract/textract"
 )
 
 func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
@@ -28,7 +28,15 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "unable to convert base64 to bytes: %s"}`+"\n", err.Error()),
 		}, nil
 	}
-	output, err := extract(b)
+	mySession, err := session.NewSession()
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 400,
+			Body:       fmt.Sprintf(`{"error": "unable to create session: %s"}`+"\n", err.Error()),
+		}, nil
+	}
+	output, err := textract.Extract(mySession, b)
 	if err != nil {
 		return &events.APIGatewayProxyResponse{
 			Headers:    map[string]string{"Content-Type": "application/json"},
@@ -36,7 +44,7 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "failed to extract: %s"}`+"\n", err.Error()),
 		}, nil
 	}
-	table, err := toTable(output)
+	table, err := textract.ToTable(output)
 	if err != nil {
 		return &events.APIGatewayProxyResponse{
 			Headers:    map[string]string{"Content-Type": "application/json"},
@@ -60,38 +68,6 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 	}, nil
 }
 
-func extract(bs []byte) (*textract.DetectDocumentTextOutput, error) {
-	mySession, err := session.NewSession()
-	if err != nil {
-		return nil, err
-	}
-	svc := textract.New(mySession)
-	input := &textract.DetectDocumentTextInput{
-		Document: &textract.Document{Bytes: bs},
-	}
-	output, err := svc.DetectDocumentText(input)
-	if err != nil {
-		return nil, err
-	}
-	return output, nil
-}
-
 func main() {
 	lambda.Start(HandleRequest)
 }
-
-/*
-
-	// write table to stdout
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	for _, row := range table {
-		for j, cell := range row {
-			fmt.Fprintf(w, cell)
-			if j < len(row)-1 {
-				fmt.Fprintf(w, "\t")
-			}
-		}
-		fmt.Fprintf(w, "\n")
-	}
-	w.Flush()
-*/
--- ocr.go
+++ ocr.go
@@ -1,25 +1,20 @@
-package main
+package extract
 
 import (
-	"log"
-	"math"
 	"sort"
-	"strings"
-
-	"github.com/aws/aws-sdk-go/service/textract"
 )
 
 var awsRegion string
 
-type word struct {
-	text    string
-	xLeft   float64
-	xRight  float64
-	yTop    float64
-	yBottom float64
+type Word struct {
+	Text    string
+	LeftX   float64
+	RightX  float64
+	TopY    float64
+	BottomY float64
 }
 
-type byXLeft []word
+type byXLeft []Word
 
 func (s byXLeft) Len() int {
 	return len(s)
@@ -28,10 +23,10 @@ func (s byXLeft) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 func (s byXLeft) Less(i, j int) bool {
-	return s[i].xLeft < s[j].xLeft
+	return s[i].LeftX < s[j].LeftX
 }
 
-type byRow []word
+type byRow []Word
 
 func (s byRow) Len() int {
 	return len(s)
@@ -44,55 +39,55 @@ func (s byRow) Less(i, j int) bool {
 	// find row by checking if the bottom y is above the top y.
 	// within a row, use xLeft
 
-	if s[i].yBottom < s[j].yTop {
+	if s[i].BottomY < s[j].TopY {
 		return true // i is on row before j
 	}
-	if s[i].yTop > s[j].yBottom {
+	if s[i].TopY > s[j].BottomY {
 		return false // j is on row before i
 	}
 
 	// same row, so compare x
-	return s[i].xLeft < s[j].xLeft
+	return s[i].LeftX < s[j].LeftX
 }
 
-type bySize [][2]float64
+type BySize [][2]float64
 
-func (s bySize) Len() int {
+func (s BySize) Len() int {
 	return len(s)
 }
-func (s bySize) Swap(i, j int) {
+func (s BySize) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
-func (s bySize) Less(i, j int) bool {
+func (s BySize) Less(i, j int) bool {
 	return s[i][1]-s[i][0] < s[j][1]-s[j][0]
 }
 
 // words need to be sorted by xLeft
-func findGroups(words []word) [][2]float64 {
+func FindGroups(words []Word) [][2]float64 {
 	// sort words by xleft
 	sort.Sort(byXLeft(words))
 	splits := make([][2]float64, 0)
 	xRight := float64(0)
 	for i, w := range words {
-		if w.xLeft > xRight && i > 0 {
-			splits = append(splits, [2]float64{xRight, w.xLeft})
+		if w.LeftX > xRight && i > 0 {
+			splits = append(splits, [2]float64{xRight, w.LeftX})
 		}
-		xRight = w.xRight
+		xRight = w.RightX
 	}
-	sort.Sort(bySize(splits))
+	sort.Sort(BySize(splits))
 	return splits
 }
 
-func splitRowBoxes(words []word, xs []float64) [][]word {
+func SplitRowBoxes(words []Word, xs []float64) [][]Word {
 	sort.Sort(byXLeft(words))
-	partitions := make([][]word, len(xs)+1)
+	partitions := make([][]Word, len(xs)+1)
 	for i := range partitions {
-		partitions[i] = make([]word, 0)
+		partitions[i] = make([]Word, 0)
 	}
 
 	i := 0
 	for _, word := range words {
-		if i < len(xs) && word.xLeft > xs[i] {
+		if i < len(xs) && word.LeftX > xs[i] {
 			i++
 		}
 		partitions[i] = append(partitions[i], word)
@@ -102,87 +97,25 @@ func splitRowBoxes(words []word, xs []float64) [][]word {
 
 // words need to be sorted by row order,
 // assume one row has max(yBottom) < min(yMax) other row
-func partitionIntoRows(words []word) [][]word {
+func PartitionIntoRows(words []Word) [][]Word {
 	// sort by row
 	sort.Sort(byRow(words))
 
-	partitions := make([][]word, 0)
-	firstRow := make([]word, 0)
+	partitions := make([][]Word, 0)
+	firstRow := make([]Word, 0)
 	firstRow = append(firstRow, words[0])
 	partitions = append(partitions, firstRow)
 	i := 0
 	prevX := float64(0)
 	for _, w := range words[1:] {
 		// if new row
-		if w.xLeft < prevX {
+		if w.LeftX < prevX {
 			i++
-			newRow := make([]word, 0)
+			newRow := make([]Word, 0)
 			partitions = append(partitions, newRow)
 		}
 		partitions[i] = append(partitions[i], w)
-		prevX = w.xLeft
+		prevX = w.LeftX
 	}
 	return partitions
-}
-
-func toTable(output *textract.DetectDocumentTextOutput) ([][]string, error) {
-	words := make([]word, 0)
-	for _, block := range output.Blocks {
-		if *block.BlockType != "WORD" {
-			continue
-		}
-		var w word
-		w.text = *block.Text
-		/*
-			Coordinate system
-				topLeft := {x: 0, y: 0}
-				topRight := {x: 0, y: 1}
-				bottomLeft := {x: 1, y: 0}
-				bottomRight := {x: 1, y: 1}
-		*/
-		w.xLeft = 1
-		w.yTop = 1
-		for _, boundingBox := range block.Geometry.Polygon {
-			w.xLeft = math.Min(w.xLeft, *boundingBox.X)
-			w.xRight = math.Max(w.xRight, *boundingBox.X)
-			w.yTop = math.Min(w.yTop, *boundingBox.Y)
-			w.yBottom = math.Max(w.yBottom, *boundingBox.Y)
-		}
-		words = append(words, w)
-	}
-
-	// find partitions
-	intervals := findGroups(words)
-	sort.Sort(bySize(intervals))
-	log.Println(intervals)
-	splitAt := make([]float64, len(intervals))
-	for i, interval := range intervals {
-		splitAt[i] = interval[0] + ((interval[1] - interval[0]) / 2)
-	}
-	// how many columns?
-	nColumns := 3
-	nSplits := nColumns - 1
-	splitAt = splitAt[:nSplits]
-	sort.Float64s(splitAt)
-
-	rows := partitionIntoRows(words)
-
-	// initialize table
-	table := make([][]string, len(rows))
-	for i := range rows {
-		table[i] = make([]string, len(splitAt)+1)
-	}
-
-	// populate table
-	for i, rowBoxes := range rows {
-		cellsBoxes := splitRowBoxes(rowBoxes, splitAt)
-		for j, cell := range cellsBoxes {
-			wordsInCell := make([]string, len(cell))
-			for k, w := range cell {
-				wordsInCell[k] = w.text
-			}
-			table[i][j] = strings.TrimSpace(strings.Join(wordsInCell, " "))
-		}
-	}
-	return table, nil
 }
