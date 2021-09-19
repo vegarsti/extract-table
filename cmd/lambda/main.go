ca1e9988237888ccf5a51e2c7acad15b74e2b050
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-09-19 20:36:20 +0200

    Reduce repetition with response helpers (#5)

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -16,84 +16,60 @@ import (
 
 func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	if !req.IsBase64Encoded {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       `{"error": "request body must have a content-type that is either image/png or image/jpeg"}` + "\n",
-		}, nil
+		return errorResponse(fmt.Errorf("request body must have a content-type that is either image/png or image/jpeg")), nil
 	}
-	b, err := base64.StdEncoding.DecodeString(req.Body)
+	imageBytes, err := base64.StdEncoding.DecodeString(req.Body)
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "unable to convert base64 to bytes: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("unable to convert base64 to bytes: %w", err)), nil
 	}
-	mySession, err := session.NewSession()
+	sess, err := session.NewSession()
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "unable to create session: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("unable to create session: %w", err)), nil
 	}
 
 	// Check if table is stored
-	checksum := md5.Sum(b)
-	storedBytes, err := dynamodb.GetTable(mySession, checksum[:])
+	checksum := md5.Sum(imageBytes)
+	storedBytes, err := dynamodb.GetTable(sess, checksum[:])
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "dynamodb.GetTable: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("dynamodb.GetTable: %w", err)), nil
 	}
 	if storedBytes != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 200,
-			Body:       string(storedBytes) + "\n",
-		}, nil
+		return successResponse(storedBytes), nil
 	}
 
-	output, err := textract.Extract(mySession, b)
+	output, err := textract.Extract(sess, imageBytes)
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "failed to extract: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("failed to extract: %w", err)), nil
 	}
 	table, err := textract.ToTableFromDetectedTable(output)
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "failed to convert to table: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("failed to convert to table: %w", err)), nil
 	}
 
 	tableBytes, err := json.MarshalIndent(table, "", "  ")
 	if err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "failed to convert to json: %s"}`+"\n", err.Error()),
-		}, nil
+		return errorResponse(fmt.Errorf("failed to convert to json: %w", err)), nil
 	}
-	if err := dynamodb.PutTable(mySession, checksum[:], tableBytes); err != nil {
-		return &events.APIGatewayProxyResponse{
-			Headers:    map[string]string{"Content-Type": "application/json"},
-			StatusCode: 400,
-			Body:       fmt.Sprintf(`{"error": "dynamodb.PutTable: %s"}`+"\n", err.Error()),
-		}, nil
+	if err := dynamodb.PutTable(sess, checksum[:], tableBytes); err != nil {
+		return errorResponse(fmt.Errorf("dynamodb.PutTable: %w", err)), nil
 	}
+	return successResponse(tableBytes), nil
+}
+
+func errorResponse(err error) *events.APIGatewayProxyResponse {
+	return &events.APIGatewayProxyResponse{
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		StatusCode: 400,
+		Body:       fmt.Sprintf(`{"error": "%s"}`+"\n", err.Error()),
+	}
+}
+
+func successResponse(tableBytes []byte) *events.APIGatewayProxyResponse {
 	return &events.APIGatewayProxyResponse{
 		Headers:    map[string]string{"Content-Type": "application/json"},
 		StatusCode: 200,
 		Body:       string(tableBytes) + "\n",
-	}, nil
+	}
 }
 
 func main() {
