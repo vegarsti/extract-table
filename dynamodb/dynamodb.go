2756087914afb5214bd1c42d9fabe72993a9acbf
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-09-19 20:00:17 +0200

    Add dynamodb package

---
+++ dynamodb/dynamodb.go
@@ -0,0 +1,71 @@
+package dynamodb
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func CreateTable(sess *session.Session) error {
+	tableName := "Tables"
+	billingMode := "PAY_PER_REQUEST"
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Checksum"),
+				AttributeType: aws.String("B"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Checksum"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		TableName:   aws.String(tableName),
+		BillingMode: &billingMode,
+	}
+	svc := dynamodb.New(sess)
+	if _, err := svc.CreateTable(input); err != nil && err.Error() != fmt.Sprintf("ResourceInUseException: Table already exists: %s", tableName) {
+		return fmt.Errorf("create table: %w", err)
+	}
+	return nil
+}
+
+func PutTable(sess *session.Session, checksum []byte, table []byte) error {
+	svc := dynamodb.New(sess)
+	putInput := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"Checksum":  {B: checksum},
+			"JSONTable": {B: table},
+		},
+		TableName: aws.String("Tables"),
+	}
+	if _, err := svc.PutItem(putInput); err != nil {
+		return fmt.Errorf("put item: %w", err)
+	}
+	return nil
+}
+
+func GetTable(sess *session.Session, checksum []byte) ([]byte, error) {
+	svc := dynamodb.New(sess)
+	projection := "JSONTable"
+	putInput := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"Checksum": {B: checksum},
+		},
+		ProjectionExpression: &projection,
+		TableName:            aws.String("Tables"),
+	}
+	output, err := svc.GetItem(putInput)
+	if err != nil {
+		return nil, fmt.Errorf("get item: %w", err)
+	}
+	table, ok := output.Item["JSONTable"]
+	if !ok {
+		return nil, nil
+	}
+	return table.B, nil
+}
