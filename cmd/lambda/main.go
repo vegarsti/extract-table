815df5dd7ffe03fc7688803ec0a68aab3d4398ad
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-09-19 20:00:57 +0200

    Use dynamodb package in cli and lambda commands

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 
 	"encoding/base64"
@@ -9,6 +10,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/vegarsti/extract/dynamodb"
 	"github.com/vegarsti/extract/textract"
 )
 
@@ -36,6 +38,25 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "unable to create session: %s"}`+"\n", err.Error()),
 		}, nil
 	}
+
+	// Check if table is stored
+	checksum := md5.Sum(b)
+	storedBytes, err := dynamodb.GetTable(mySession, checksum[:])
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 400,
+			Body:       fmt.Sprintf(`{"error": "dynamodb.GetTable: %s"}`+"\n", err.Error()),
+		}, nil
+	}
+	if storedBytes != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 200,
+			Body:       string(storedBytes) + "\n",
+		}, nil
+	}
+
 	output, err := textract.Extract(mySession, b)
 	if err != nil {
 		return &events.APIGatewayProxyResponse{
@@ -52,7 +73,8 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "failed to convert to table: %s"}`+"\n", err.Error()),
 		}, nil
 	}
-	bodyBytes, err := json.MarshalIndent(table, "", "  ")
+
+	tableBytes, err := json.MarshalIndent(table, "", "  ")
 	if err != nil {
 		return &events.APIGatewayProxyResponse{
 			Headers:    map[string]string{"Content-Type": "application/json"},
@@ -60,10 +82,17 @@ func HandleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRe
 			Body:       fmt.Sprintf(`{"error": "failed to convert to json: %s"}`+"\n", err.Error()),
 		}, nil
 	}
+	if err := dynamodb.PutTable(mySession, checksum[:], tableBytes); err != nil {
+		return &events.APIGatewayProxyResponse{
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			StatusCode: 400,
+			Body:       fmt.Sprintf(`{"error": "dynamodb.PutTable: %s"}`+"\n", err.Error()),
+		}, nil
+	}
 	return &events.APIGatewayProxyResponse{
 		Headers:    map[string]string{"Content-Type": "application/json"},
 		StatusCode: 200,
-		Body:       string(bodyBytes) + "\n",
+		Body:       string(tableBytes) + "\n",
 	}, nil
 }
 
